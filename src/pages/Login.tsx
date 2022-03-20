ad2f32ffdb5765daecc7c2d2612412978e1c7355
pacheco <andrephenrique@hotmail.com>
2022-03-20 20:34:49 -0300

    wip private route

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const LoginPage: FC<{}> = () => {
+
+    return (
+        <h1>Login</h1>
+    )
+
+}
+
+export default LoginPage
---
+++ src/router/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+
+function PrivateRoute({ children }: { children: JSX.Element }) {
+  let auth = true;
+  // https://stackblitz.com/github/remix-run/react-router/tree/main/examples/auth?file=src/App.tsx
+
+  if (!auth) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
+export default PrivateRoute;
