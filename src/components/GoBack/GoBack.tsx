46fc1c25a3b6258e9aeec70d8774edd907e1a48f
pacheco <andrephenrique@hotmail.com>
2022-09-04 00:13:08 -0300

    warnings cleanup

--- src/components/GoBack/GoBack.tsx
+++ src/components/GoBack/GoBack.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { KeyboardBackspace as Back } from "@mui/icons-material";
 import { Wrapper } from "./styles";
 
--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import { FC, InputHTMLAttributes, TextareaHTMLAttributes } from "react";
+import { FC, TextareaHTMLAttributes } from "react";
 import { Label, TextArea, Wrapper } from "./styles";
 
 interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
